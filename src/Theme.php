a6ac3cb2b029a27bb69a3f0ade9bf976401c54d9
lgolabek <lgolabek@outlook.com>
2022-12-12 18:40:23 +0100

    1.0.0

--- src/Theme.php
+++ src/Theme.php
@@ -13,7 +13,12 @@ class Theme
 
     public function __construct()
     {
-        $theme = file_get_contents(SITE_URL . "/theme/theme.json");
-        return json_decode($theme, true);
+        $theme_file = file_get_contents(SITE_URL . "/theme/theme.json");
+        $theme_data = json_decode($theme_file, true);
+        $this->name = $theme_data['name'];
+        $this->version = $theme_data['version'];
+        $this->author = $theme_data['author'];
+        $this->url = $theme_data['url'];
+        $this->description = $theme_data['description'];
     }
 }
