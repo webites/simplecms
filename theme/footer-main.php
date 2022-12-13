4934d5494b7e68d2334da3ef653f99a5e8cebb5a
lgolabek <lgolabek@outlook.com>
2022-12-13 14:59:15 +0100

    1.0.0

--- theme/footer-main.php
+++ theme/footer-main.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-6">
-                Logo firmy
+                <img src="<?php echo $site->getLogoUrl(); ?>" alt="">
             </div>
             <div class="col-12 col-lg-6">
                 Footer menu
