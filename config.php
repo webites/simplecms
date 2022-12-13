f79ee72964247f7af8613716a6eb16aaf1e01707
lgolabek <lgolabek@outlook.com>
2022-12-13 19:39:37 +0100

    1.0.0

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 
-use voku\helper\Hooks;
 
 const DB_DATABASE = 'adstudio_simplecms';
 const DB_USER = 'adstudio_simplecms';
@@ -10,12 +9,3 @@ const DB_CHARSET = 'utf8mb4_unicode_ci';
 
 
 const SITE_URL = 'https://dev.simplecms.webites.dev';
-
-
-$hooks = Hooks::getInstance();
-$hooks->add_action('simple_header', 'echo_this_in_header');
-
-function echo_this_in_header()
-{
-    echo "test";
-}
