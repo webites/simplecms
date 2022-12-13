892f19a34efcf19c387d1095f876c766467556ae
lgolabek <lgolabek@outlook.com>
2022-12-13 14:49:12 +0100

    1.0.0

--- theme/header-main.php
+++ theme/header-main.php
@@ -13,7 +13,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-5">
-                <img src="<?php //echo $site->getLogoUrl(); 
+                <img src="<?php echo $site->getLogoUrl();
                             ?>" alt="">
             </div>
             <div class="col-12 col-lg-7">
