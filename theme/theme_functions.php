ce71091b96a6902cda849208e50d26d14af6ddb4
lgolabek <lgolabek@outlook.com>
2022-12-13 19:50:00 +0100

    1.0.0

--- theme/theme_functions.php
+++ theme/theme_functions.php
@@ -1,11 +1,13 @@
 <?php
 
+use Simple\Core\Page;
 use voku\helper\Hooks;
 
 $hooks = Hooks::getInstance();
 $hooks->add_action('simple_header', 'echo_this_in_header');
 
-function echo_this_in_header($page)
+function echo_this_in_header($pageId)
 {
+    $page = new Page($pageId);
     echo $page['title'];
 }
