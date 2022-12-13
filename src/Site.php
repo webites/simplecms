9a4ca2dc065349246883f1d58ad88c22ba9a736b
lgolabek <lgolabek@outlook.com>
2022-12-13 14:52:39 +0100

    1.0.0

--- src/Site.php
+++ src/Site.php
@@ -16,8 +16,8 @@ class Site
             $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";charset=utf8", DB_USER, DB_PASSWORD);
 
             $data = $dbh->query('SELECT * FROM site_settings');
-            $record = $data->fetchAll();
-            $this->logo = $record['logo'];
+            $record = $data->fetch();
+            $this->logo = $record['name'];
             $dbh = null;
         } catch (PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
