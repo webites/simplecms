7927918b0bd483bba8e7b934f6bfb1f70d76a3a1
lgolabek <lgolabek@outlook.com>
2022-12-14 16:49:27 +0100

    1.0.0

--- admin/dashboard/global.php
+++ admin/dashboard/global.php
@@ -58,13 +58,16 @@ $sign = substr($username, 0, 1);
                         <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
                             <form action="<?php SITE_URL ?>/admin/global-update" class="dashboard__global__form">
                                 <h4 id="simple-list-item-1">Basic site info</h4>
-                                <?php
-                                foreach ($site as $item => $value) {
-                                    $label = str_replace("_", " ", $item);
-                                ?>
-                                    <label for="<?php echo $item ?>"><?php echo $label ?></label>
-                                    <input type="text" id="<?php echo $item ?>" name="<?php echo $item ?>" value="<?php echo $value ?>">
-                                <?php } ?>
+                                <div class="dashboard__global__form__section">
+                                    <?php
+                                    foreach ($site as $item => $value) {
+                                        $label = str_replace("_", " ", $item);
+                                    ?>
+                                        <label for="<?php echo $item ?>"><?php echo $label ?></label>
+                                        <input type="text" id="<?php echo $item ?>" name="<?php echo $item ?>" value="<?php echo $value ?>">
+                                    <?php } ?>
+
+                                </div>
 
                                 <h4 id="simple-list-item-2">Item 2</h4>
                                 <p>...</p>
