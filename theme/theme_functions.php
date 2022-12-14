27123c39b4b0e19c181582dfa87e1499d74b2b8a
lgolabek <lgolabek@outlook.com>
2022-12-14 10:54:34 +0100

    1.0.0

--- theme/theme_functions.php
+++ theme/theme_functions.php
@@ -8,6 +8,5 @@ $hooks->add_action('simple_header', 'echo_this_in_header');
 
 function echo_this_in_header($pageId)
 {
-    $page = new Page($pageId);
-    echo $page['title'];
+    echo "Title";
 }
