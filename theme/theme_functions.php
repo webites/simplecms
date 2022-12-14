03358b81fe9413ea61aa360e5acf7bb458f61ded
lgolabek <lgolabek@outlook.com>
2022-12-14 11:12:25 +0100

    1.0.0

--- theme/theme_functions.php
+++ theme/theme_functions.php
@@ -6,7 +6,7 @@ use voku\helper\Hooks;
 $hooks = Hooks::getInstance();
 $hooks->add_action('simple_header', 'echo_this_in_header');
 
-function echo_this_in_header($pageId)
+function echo_this_in_header($page)
 {
-    echo "Title";
+    echo $page['title'];
 }
