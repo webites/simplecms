f83c9aff2d2d9bca699d80390634653f363dc2d0
lgolabek <lgolabek@outlook.com>
2022-12-28 14:29:56 +0100

    1.0.0

--- src/Notification/Alert.php
+++ src/Notification/Alert.php
@@ -9,11 +9,13 @@ class Alert
     protected $redirect;
     public $message;
 
-    public function __construct(string $redirect, string $message)
+    public function __construct(string $redirect, string $message, string $type = 'info')
     {
         $this->redirect = $redirect;
         $this->message = $message;
+        $this->type = $type;
         $_SESSION['alert'] = $this->message;
+        $_SESSION['type'] = $this->type;
         header('Location: ' . $this->redirect);
     }
 
