3b68b2aa79e7a99949a4fcba63e72f8f2b56da33
lgolabek <lgolabek@outlook.com>
2022-12-28 16:37:38 +0100

    1.0.0

--- admin/assets/js/notify/script.js
+++ admin/assets/js/notify/script.js
@@ -1,13 +1,13 @@
-const btnClose = document.getElementById("dashboard__notification__close");
-const modal = document.querySelector(".dashboard__notification");
-btnClose.addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
 const btnDelete = document.querySelectorAll(".pages__actions__button--delete");
 
 btnDelete.forEach(function (e) {
   e.addEventListener("click", function () {
     document.querySelector(".pages__actions__delete").style.display = "block";
   });
+});
+
+const btnClose = document.getElementById("dashboard__notification__close");
+const modal = document.querySelector(".dashboard__notification");
+btnClose.addEventListener("click", function () {
+  modal.style.display = "none";
 });
