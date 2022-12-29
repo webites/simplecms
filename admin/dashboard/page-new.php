3b67565b0e1a224ee5e69858d65d38aef1fbf3b6
lgolabek <lgolabek@outlook.com>
2022-12-29 11:01:41 +0100

    1.0.0

--- admin/dashboard/page-new.php
+++ admin/dashboard/page-new.php
@@ -57,36 +57,7 @@ $sign = substr($username, 0, 1);
             </div>
         </div>
     </div>
-    <link href='https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css' rel='stylesheet' type='text/css' />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/plugins/image.min.css">
-    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js'></script>
-    <script>
-        var editor = new FroalaEditor('#content', {
-            fileUploadURL: '/images',
-            fileUseSelectedText: true,
-            imageManagerLoadURL: '/images',
-            imageUploadRemoteUrls: false,
-            toolbarButtons: {
-                'moreText': {
-                    'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
-                },
-                'moreParagraph': {
-                    'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
-                },
-                'moreRich': {
-                    'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertHR']
-                },
-                'moreMisc': {
-                    'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help']
-                }
-            },
-            imageInsertButtons: ['imageBack', '|', 'imageByURL'],
-            videoInsertButtons: ['videoBack', '|', 'videoByURL', 'videoEmbed'],
-            fontFamilySelection: true,
-            fontSizeSelection: true,
-            paragraphFormatSelection: true
-        });
-    </script>
+    <?php wyswig_loader(); ?>
 </body>
 
 </html>
