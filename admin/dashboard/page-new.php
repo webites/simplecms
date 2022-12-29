f74b0f86be047a2f84009dff5300f949a55e4b1b
lgolabek <lgolabek@outlook.com>
2022-12-29 10:03:29 +0100

    1.0.0

--- admin/dashboard/page-new.php
+++ admin/dashboard/page-new.php
@@ -16,8 +16,8 @@ $sign = substr($username, 0, 1);
     <link rel="stylesheet" href="<?php echo SITE_URL ?>/admin/assets/css/style.css" />
     <link rel="stylesheet" href="<?php echo SITE_URL ?>/admin/assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?php echo SITE_URL ?>/admin/assets/node_modules/bootstrap-icons/font/bootstrap-icons.css" />
+    <link href='https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css' rel='stylesheet' type='text/css' />
 
-    <script src="https://cdn.ckeditor.com/ckeditor5/35.4.0/classic/ckeditor.js"></script>
 </head>
 
 <body>
@@ -57,14 +57,10 @@ $sign = substr($username, 0, 1);
             </div>
         </div>
     </div>
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js'></script>
 
     <script>
-        ClassicEditor
-            .create(document.querySelector('#content'), {
-                image: {
-                    toolbar: ['toggleImageCaption', 'imageTextAlternative']
-                }
-            })
+        var editor = new FroalaEditor('#content');
     </script>
 </body>
 
