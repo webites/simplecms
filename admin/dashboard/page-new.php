e70908e371ca7ff21b384d6f73eed6e1ceed004a
lgolabek <lgolabek@outlook.com>
2022-12-29 10:44:10 +0100

    1.0.0

--- admin/dashboard/page-new.php
+++ admin/dashboard/page-new.php
@@ -65,10 +65,20 @@ $sign = substr($username, 0, 1);
             fileUseSelectedText: true,
             imageManagerLoadURL: '/images',
             imageUploadRemoteUrls: false,
-            toolbarButtons: [
-                ['fontFamily', '|', 'fontSize', '|', 'paragraphFormat', '|', 'bold', 'italic', 'underline', 'textColor', 'backgroundColor', 'undo', 'redo', 'codeView'],
-                ['inlineClass', 'inlineStyle', 'clearFormatting']
-            ],
+            toolbarButtons: {
+                'moreText': {
+                    'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+                },
+                'moreParagraph': {
+                    'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+                },
+                'moreRich': {
+                    'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertHR']
+                },
+                'moreMisc': {
+                    'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help']
+                }
+            },
             fontFamilySelection: true,
             fontSizeSelection: true,
             paragraphFormatSelection: true
