a27eea372542d9c263c71edd5f0a30b5a8302cb2
lgolabek <lgolabek@outlook.com>
2022-12-29 16:27:50 +0100

    1.0.0

--- modules/Manager.php
+++ modules/Manager.php
@@ -13,7 +13,8 @@ class Manager
         $current_modules = [];
         foreach ($modules as $module) {
             if (!str_contains($module, '.') || !str_contains($module, '..')) {
-                $current_modules .= $module;
+                // $current_modules .= $module;
+                array_push($current_modules, $module);
             }
         }
         var_dump($current_modules);
