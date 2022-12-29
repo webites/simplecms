78b0d415b5d37f3d2b70800aae72d079660b430b
lgolabek <lgolabek@outlook.com>
2022-12-29 16:25:02 +0100

    1.0.0

--- modules/Manager.php
+++ modules/Manager.php
@@ -10,12 +10,12 @@ class Manager
     static function getModules()
     {
         $modules = scandir('./modules');
-        // $current_modules = [];
-        // foreach ($modules as $module) {
-        //     if (str_contains($module, '.') === false || str_contains($module, '..') === false) {
-        //         $current_modules .= $module;
-        //     }
-        // }
-        var_dump($modules);
+        $current_modules = [];
+        foreach ($modules as $module) {
+            if (str_contains($module, '.') === false || str_contains($module, '..') === false) {
+                $current_modules .= $module;
+            }
+        }
+        var_dump($current_modules);
     }
 }
