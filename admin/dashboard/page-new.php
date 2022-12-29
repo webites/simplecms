bd9c6a57a781dc715605e2c4f54988e617ed6cf1
lgolabek <lgolabek@outlook.com>
2022-12-29 10:06:06 +0100

    1.0.0

--- admin/dashboard/page-new.php
+++ admin/dashboard/page-new.php
@@ -60,7 +60,9 @@ $sign = substr($username, 0, 1);
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js'></script>
 
     <script>
-        var editor = new FroalaEditor('#content');
+        var editor = new FroalaEditor('#content', {
+            fileUploadURL: '/images'
+        });
     </script>
 </body>
 
