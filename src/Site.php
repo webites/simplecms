03672cef3e466117196710073792c45d0669423e
lgolabek <lgolabek@outlook.com>
2022-12-30 11:41:40 +0100

    1.0.0

--- src/Site.php
+++ src/Site.php
@@ -59,6 +59,8 @@ class Site
     {
         $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";charset=utf8", DB_USER, DB_PASSWORD);
         foreach ($settings as $setting => $value) {
+            $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";charset=utf8", DB_USER, DB_PASSWORD);
+
             var_dump($setting, $value);
             $sql = "UPDATE site_settings SET value=? WHERE name='" . $setting . "'";
             $stmt = $dbh->prepare($sql);
